c794310aa23746cce4adaf11d30e13bed82a71bd
ShineEnoki <thuriyac4@gmail.com>
2023-04-19 23:27:22 +0630

    changing techs i use

--- pages/experience.jsx
+++ pages/experience.jsx
@@ -3,6 +3,56 @@ import React from 'react';
 
 import { BsPatchCheckFill } from 'react-icons/bs';
 
+const frontendTechs = [
+    {
+        name: "HTML",
+        description: 'A markup language used to create web pages and applications'
+    },
+    {
+        name: "CSS",
+        description: 'To style web pages'
+    },
+    {
+        name: "JavaScript",
+        description: 'A programming language used for creating interactive web applications'
+    },
+    {
+        name: "React",
+        description: 'A JavaScript library used for building user interfaces'
+    },
+    {
+        name: "Bootstrap",
+        description: 'A popular framework used for creating responsive and mobile-first websites'
+    },
+    {
+        name: "Tailwind CSS",
+        description: 'A utility-first CSS framework that provides a set of pre-designed CSS classes to make building responsive user interfaces faster and easier'
+    },
+    {
+        name: "MaterialUI",
+        description: 'A popular React component library that provides pre-designed components making it easier to build attractive and consistent user interfaces'
+    },
+    {
+        name: "NextJS",
+        description: 'a popular open-source React-based framework used for building server-side rendered (SSR) and static web applications'
+    },
+
+]
+
+const TechArticle = ({tech}) => {
+    return (
+        <article className='rounded-lg flex gap-3 p-3'>
+            <div>
+                <BsPatchCheckFill className='color-primary mt-[6px] w-5 h-5' />
+            </div>
+            <div>
+                <h4> {tech.name} </h4>
+                <small> {tech.description} </small>
+            </div>
+        </article>
+    )
+}
+
 const Experience = () => {
     return (
         <section id="experience">
@@ -10,97 +60,21 @@ const Experience = () => {
             <h5>What skills I have</h5>
             <h2>My Experience</h2>
 
-            <div className="container experience__container">
-                <div className="experience__frontend">
-                    <h3>Frontend Development</h3>
-                    <div className="experience__content">
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>HTML</h4>
-                                <small className='text-light'>Experienced</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>CSS</h4>
-                                <small className='text-light'>Experienced</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>JavaScript</h4>
-                                <small className='text-light'>Experienced</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>Bootstrap</h4>
-                                <small className='text-light'>Experienced</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>React</h4>
-                                <small className='text-light'>Experienced</small>
-                            </div>
-                        </article>
+            <div className="container ">
+                <div className="bg-variant p-5 rounded-3xl transition hover:bg-transparent">
+                    <h3 className='text-center color-primary text-xl mb-5'>Frontend Development Tech</h3>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                        
+                        {
+                            frontendTechs.map( tech => 
+                                <TechArticle tech={tech} key={tech.name} />
+                            )
+                        }
+
                     </div>
                 </div>
                 {/* END OF FRONT END */}
 
-
-                <div className="experience__backend">
-                    <h3>Backend Development</h3>
-                    <div className="experience__content">
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>Node JS</h4>
-                                <small className='text-light'>Intermediate</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>Express</h4>
-                                <small className='text-light'>Intermediate</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>Mongo DB</h4>
-                                <small className='text-light'>Intermediate</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>php</h4>
-                                <small className='text-light'>Beginner</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>laravel</h4>
-                                <small className='text-light'>Beginner</small>
-                            </div>
-                        </article>
-                        <article className='experience__details'>
-                            <BsPatchCheckFill className='experience__details-icon'/>
-                            <div>
-                                <h4>MySQL</h4>
-                                <small className='text-light'>Beginner</small>
-                            </div>
-                        </article>
-                    </div>
-                </div>
             </div>
         </section>
     )

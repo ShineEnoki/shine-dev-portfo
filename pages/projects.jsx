31dae2acda86a4f3793be5e0ebf9e6fba694f5bc
ShineEnoki <thuriyac4@gmail.com>
2023-05-06 18:37:29 +0630

    change project section a little

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -4,61 +4,68 @@ import React from 'react';
 import Image from 'next/image';
 
 //importing assets
+import myLoli from '../public/assets/my-loli.png';
 import BookShop from '../public/assets/hope-book-shop.png'
 import project2 from '../public/assets/portfolio2.jpg';
 import project3 from '../public/assets/portfolio3.jpg';
 import project4 from '../public/assets/portfolio4.jpg';
 
+const projects = [
+    {
+        id: 1,
+        name: 'My-Loli',
+        src: myLoli,
+        description: "What you have to do? What's on your mind? Loli, Your personal assistant is here for you.Let's me save your memory",
+        githubLink: "https://github.com/ShineEnoki/my-loli-source-code",
+        demoLink: 'https://my-loli.vercel.app/'
+    },
+    {
+        id: 2,
+        name: 'Hope Book Shop',
+        src: BookShop,
+        description: "Books are the plane, and the train, and the road. They are the destination, and the journey. They are home.Here is the hope",
+        githubLink: "https://github.com/ShineEnoki/react-book-shop-app",
+        demoLink: 'https://my-loli.vercel.app/'
+    },
+
+]
+
+const Project = ({ project }) => {
+    const { name, src, description, githubLink, demoLink } = project
+    return (
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <Image src={src} alt="" />
+            </div>
+            <h3> {name} </h3>
+            <p className='portfolio__item-description'> {description} </p>
+            <div className='flex gap-1'>
+                <a href={githubLink} className='btn' target='_blank' rel="noreferrer">Github</a>
+                <a href={demoLink} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+            </div>
+
+        </article>
+    )
+}
+
 const Portfolio = () => {
-  return (
-    <section id='portfolio'>
-        <Nav />
-        <h5>My Work</h5>
-        <h2>Portfolio</h2>
-        
-        <div className="container portfolio__container">
-            <article className='portfolio__item'>
-                <div className="portfolio__item-image">
-                    <Image src={BookShop} alt="" />
-                </div>
-                <h3>Hope Book Shop</h3>
-                <p className='portfolio__item-description'>This is the React book shop app with google book api.</p>
-                <a href="https://github.com/ShineEnoki/react-book-shop-app" className='btn' target='_blank' rel="noreferrer">Github</a>
-                <a href="https://shineenoki.github.io/react-book-shop-app/" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-            </article>
+    return (
+        <section id='portfolio'>
+            <Nav />
+            <h5>My Work</h5>
+            <h2>Portfolio</h2>
 
-            <article className='portfolio__item'>
-                <div className="portfolio__item-image">
-                    <Image src={project2} alt="" />
-                </div>
-                <h3>Instagram clone with functionality</h3>
-                <a href="https//githublink" className='btn'>Github</a>
-                <a href="https//githublink" className='btn btn-primary' target='_blank'>Live Demo</a>
-            </article>
-
-            <article className='portfolio__item'>
-                <div className="portfolio__item-image">
-                    <Image 
-                        src={project3}
-                         alt="" 
-                    />
-                </div>
-                <h3>This is a portfolio item title</h3>
-                <a href="https//githublink" className='btn'>Github</a>
-                <a href="https//githublink" className='btn btn-primary' target='_blank'>Live Demo</a>
-            </article>
-
-            <article className='portfolio__item'>
-                <div className="portfolio__item-image">
-                    <Image src={project4} alt=""  l/>
-                </div>
-                <h3>This is a portfolio item title</h3>
-                <a href="https//githublink" className='btn'>Github</a>
-                <a href="https//githublink" className='btn btn-primary' target='_blank'>Live Demo</a>
-            </article>
-        </div>
-    </section>
-  )
+            <div className="container portfolio__container">
+                {
+                    projects.map((project) => {
+                        return (
+                            <Project project={project} key={project.id} />
+                        )
+                    })
+                }
+            </div>
+        </section>
+    )
 }
 
 export default Portfolio;
--- pages/services.jsx
+++ pages/services.jsx
@@ -1,46 +1,60 @@
 import Nav from '@/components/Nav';
 import React from 'react';
+import { BiCheck } from "react-icons/bi";
 
+const skills = [
+    {
+        id: 1,
+        details: "HTML/CSS (including experience with CSS preprocessors such as (Sass/SCSS)"
+    },
+    {
+        id: 2,
+        details: "Strong understanding of JavaScript (including ES6+ syntax and working knowledge of functional programming concepts)"
+    },
+    {
+        id: 3,
+        details: "Proficient in React (including experience with React concepts, React Hooks, and Context API)"
+    },
+    {
+        id: 4,
+        details: "Experience with modern frontend frameworks and libraries (such as Bootstrap, Material-UI, or Tailwind CSS)"
+    },
+    {
+        id: 5,
+        details: "Understanding of web development concepts and terminology (such as HTTP, APIs, DOM, etc.)"
+    },
+]
 
-import { BiCheck } from "react-icons/bi"
+const Skill = ({skill}) => {
+    return (
+        <li className='flex gap-1'>
+            <p className='mt-3 text-[20px]'>
+                *
+            </p>
+            <p className='px-3 py-2'> 
+                { skill }
+            </p>
+        </li>
+    )
+}
 
 const Services = () => {
     return (
         <section id="services">
             <Nav />
-            <h5>What I Offer</h5>
-            <h2>Skills</h2>
+            <h1 className='text-center'>Skills</h1>
 
-            <div className="container services__container">
+            <div className="container services__container text-white" >
                 <article className="service">
                     <div className="service__head">
                         <h3>Web Development</h3>
                     </div>
-                    <ul className='service__list'>
-                        <li>
-                            <BiCheck className='service__list-icon' />
-                            <p> Strong understanding in HTML and CSS. </p>
-                        </li>
-                        <li>
-                            <BiCheck className='service__list-icon' />
-                            <p> Experience well in Javascript and ES6 features. </p>
-                        </li>
-                        <li>
-                            <BiCheck className='service__list-icon' />
-                            <p> Develope and maintain website front end using HTML,CSS, Bootstrap and React. </p>
-                        </li>
-                        <li>
-                            <BiCheck className='service__list-icon' />
-                            <p> Working with apis from back end. </p>
-                        </li>
-                        <li>
-                            <BiCheck className='service__list-icon' />
-                            <p> Basic knowledge in back end technologies such as Php, Laravel and can use NodeJS, Express.</p>
-                        </li>
-                        <li>
-                            <BiCheck className='service__list-icon' />
-                            <p> good understanding in Database technologies such as MySql and MongoDB. </p>
-                        </li>
+                    <ul className='flex flex-col gap-2 px-3 py-2'>
+                        { skills.map((skill) => {
+                            return (
+                                <Skill key={skill.id} skill={skill.details} />
+                            )
+                        })}
                     </ul>
                 </article>
             </div>

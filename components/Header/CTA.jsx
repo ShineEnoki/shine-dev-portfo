763b637711599f83ef2a22e5e50ca65e2c9595fa
ShineEnoki <thuriyac4@gmail.com>
2023-05-09 16:35:37 +0630

    change cv v-3

--- components/Header/CTA.jsx
+++ components/Header/CTA.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const CTA = () => {
   return (
     <div className='cta'>
-      <Link href='/assets/my-resume.pdf'className='bg-variant rounded-lg p-4 hover:bg-transparent hover:border hover:border-black'>
+      <Link href='/assets/my-cv-v3.pdf'className='bg-variant rounded-lg p-4 hover:bg-transparent hover:border hover:border-black'>
         Download CV
       </Link>
 
